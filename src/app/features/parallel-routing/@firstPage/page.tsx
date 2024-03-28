3e95764f98830db1636f0a6af5fb937892941c68
Dorji Tshering <dorji.react@gmail.com>
2024-03-28 21:30:26 +0600

    feat: add useful comments

--- src/app/features/parallel-routing/@firstPage/page.tsx
+++ src/app/features/parallel-routing/@firstPage/page.tsx
@@ -1,7 +1,11 @@
 import { fakeAPI } from "@/actions/parallel-route";
 import PageContent from "./page-content";
 
-// force the page to dynamic rendering via searchParams access
+// Force the page to dynamic rendering via searchParams access
+// Using pages searchParams prop will opt the whole route into dynamic rendering
+// https://nextjs.org/docs/app/building-your-application/rendering/server-components#switching-to-dynamic-rendering
+// This is just for the demo, so you can refresh the page and see how 
+// page streaming works.
 const FirstPage = async ({
   searchParams,
 }: {
--- src/app/features/parallel-routing/@secondPage/page.tsx
+++ src/app/features/parallel-routing/@secondPage/page.tsx
@@ -1,7 +1,11 @@
 import { fakeAPI } from "@/actions/parallel-route";
 import ParallelRoutePageContent from "@/components/features/parallel-routing/parallel-route-content";
 
-// force the page to dynamic rendering via searchParams access
+// Force the page to dynamic rendering via searchParams access
+// Using pages searchParams prop will opt the whole route into dynamic rendering
+// https://nextjs.org/docs/app/building-your-application/rendering/server-components#switching-to-dynamic-rendering
+// This is just for the demo, so you can refresh the page and see how 
+// page streaming works.
 const SecondPage = async ({
   searchParams,
 }: {
