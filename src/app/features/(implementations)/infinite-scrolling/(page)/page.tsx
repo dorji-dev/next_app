b9339476b41ef8f3cd41b9d8374770f95623b526
Dorji Tshering <dorji.react@gmail.com>
2024-04-05 18:30:11 +0600

    feat: refactor for pagination and infinite scrolling page

---
+++ src/app/features/(implementations)/infinite-scrolling/(page)/page.tsx
@@ -0,0 +1,5 @@
+const InfiniteScrollingPage = () => {
+  return <div>InfiniteScrollingPage</div>;
+};
+
+export default InfiniteScrollingPage;
--- src/app/features/(implementations)/intercepting-routes/layout.tsx
+++ src/app/features/(implementations)/intercepting-routes/layout.tsx
@@ -14,7 +14,6 @@ const InterceptingRouteLayout = ({
   interceptionModal,
   explanation,
 }: InterceptingRouteLayoutProps) => {
-  
   return (
     <>
       {interceptionModal}
