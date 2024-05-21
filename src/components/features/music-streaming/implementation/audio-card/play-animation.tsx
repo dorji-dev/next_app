664693e5838aa7989c59bf09edc6a7fedace4c31
Dorji Tshering <dorji.react@gmail.com>
2024-05-21 20:51:46 +0600

    feat: update play animation UI

--- src/components/features/music-streaming/implementation/audio-card/play-animation.tsx
+++ src/components/features/music-streaming/implementation/audio-card/play-animation.tsx
@@ -9,21 +9,27 @@ const PlayAnimation = ({ className }: PropsPlayAnimationProps) => {
   return (
     <div className={clsx(styles.now, styles.playing, className)}>
       <span className={clsx(styles.bar, styles.n1)}>A</span>
-      <span className={clsx(styles.bar, styles.n2)}>B</span>
+      <span className={clsx(styles.bar, styles.n1)}>B</span>
       <span className={clsx(styles.bar, styles.n3)}>c</span>
-      <span className={clsx(styles.bar, styles.n4)}>D</span>
+      <span className={clsx(styles.bar, styles.n8)}>D</span>
       <span className={clsx(styles.bar, styles.n5)}>E</span>
-      <span className={clsx(styles.bar, styles.n6)}>F</span>
+      <span className={clsx(styles.bar, styles.n1)}>F</span>
       <span className={clsx(styles.bar, styles.n7)}>G</span>
       <span className={clsx(styles.bar, styles.n8)}>H</span>
       <span className={clsx(styles.bar, styles.n1)}>A</span>
-      <span className={clsx(styles.bar, styles.n2)}>B</span>
+      <span className={clsx(styles.bar, styles.n4)}>B</span>
       <span className={clsx(styles.bar, styles.n3)}>c</span>
-      <span className={clsx(styles.bar, styles.n4)}>D</span>
+      <span className={clsx(styles.bar, styles.n2)}>D</span>
       <span className={clsx(styles.bar, styles.n5)}>E</span>
-      <span className={clsx(styles.bar, styles.n6)}>F</span>
+      <span className={clsx(styles.bar, styles.n1)}>F</span>
       <span className={clsx(styles.bar, styles.n7)}>G</span>
+      <span className={clsx(styles.bar, styles.n3)}>H</span>
+      <span className={clsx(styles.bar, styles.n4)}>B</span>
+      <span className={clsx(styles.bar, styles.n3)}>c</span>
       <span className={clsx(styles.bar, styles.n8)}>H</span>
+      <span className={clsx(styles.bar, styles.n1)}>A</span>
+      <span className={clsx(styles.bar, styles.n2)}>B</span>
+      <span className={clsx(styles.bar, styles.n1)}>c</span>
     </div>
   );
 };
